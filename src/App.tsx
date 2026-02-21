3549ad9f73c2b6e2b59c43acf7e36d90cd553847
Dhanush <dhanushchowdary505@gmail.com>
2026-02-21 10:06:34 +0530

    Initial commit - React Vite project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {useState}from "react";
+function App(){
+  const [count,setCount]=useState(0);
+  const increment=():void=>{
+    setCount(count+1);
+    setCount("KLU");
+};
+  return (
+    <div>
+      <h1>React + TypeScript - 2500040083 - M Sai Dhanush</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+export default App;
